f6f5eec5ce5e6811935755dbcab75d55815c2d28
vudinhquang <vudinhquangk53@gmail.com>
2021-08-13 14:58:18 +0900

    Design

---
+++ 02-optimizeName/app-optimizeName/app-optimizeName/FormMain.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace app_optimizeName
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtYourName = new System.Windows.Forms.TextBox();
+            this.lblVi = new System.Windows.Forms.Label();
+            this.txtVi = new System.Windows.Forms.TextBox();
+            this.btnCopyVi = new System.Windows.Forms.Button();
+            this.lblEn = new System.Windows.Forms.Label();
+            this.txtEn = new System.Windows.Forms.TextBox();
+            this.btnCopyEn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(30, 13);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(106, 24);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Your Name";
+            // 
+            // txtYourName
+            // 
+            this.txtYourName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtYourName.Location = new System.Drawing.Point(167, 18);
+            this.txtYourName.Name = "txtYourName";
+            this.txtYourName.Size = new System.Drawing.Size(352, 26);
+            this.txtYourName.TabIndex = 1;
+            // 
+            // lblVi
+            // 
+            this.lblVi.AutoSize = true;
+            this.lblVi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVi.Location = new System.Drawing.Point(44, 68);
+            this.lblVi.Name = "lblVi";
+            this.lblVi.Size = new System.Drawing.Size(113, 24);
+            this.lblVi.TabIndex = 0;
+            this.lblVi.Text = "VietNamese";
+            // 
+            // txtVi
+            // 
+            this.txtVi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVi.Location = new System.Drawing.Point(167, 66);
+            this.txtVi.Name = "txtVi";
+            this.txtVi.Size = new System.Drawing.Size(237, 26);
+            this.txtVi.TabIndex = 1;
+            // 
+            // btnCopyVi
+            // 
+            this.btnCopyVi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnCopyVi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCopyVi.Location = new System.Drawing.Point(444, 66);
+            this.btnCopyVi.Name = "btnCopyVi";
+            this.btnCopyVi.Size = new System.Drawing.Size(75, 26);
+            this.btnCopyVi.TabIndex = 2;
+            this.btnCopyVi.Text = "Copy";
+            this.btnCopyVi.UseVisualStyleBackColor = false;
+            // 
+            // lblEn
+            // 
+            this.lblEn.AutoSize = true;
+            this.lblEn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEn.Location = new System.Drawing.Point(44, 123);
+            this.lblEn.Name = "lblEn";
+            this.lblEn.Size = new System.Drawing.Size(73, 24);
+            this.lblEn.TabIndex = 0;
+            this.lblEn.Text = "English";
+            // 
+            // txtEn
+            // 
+            this.txtEn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEn.Location = new System.Drawing.Point(167, 121);
+            this.txtEn.Name = "txtEn";
+            this.txtEn.Size = new System.Drawing.Size(237, 26);
+            this.txtEn.TabIndex = 1;
+            // 
+            // btnCopyEn
+            // 
+            this.btnCopyEn.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnCopyEn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCopyEn.Location = new System.Drawing.Point(444, 121);
+            this.btnCopyEn.Name = "btnCopyEn";
+            this.btnCopyEn.Size = new System.Drawing.Size(75, 26);
+            this.btnCopyEn.TabIndex = 2;
+            this.btnCopyEn.Text = "Copy";
+            this.btnCopyEn.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(167, 162);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Chuyển đổi";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(319, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 48);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Làm mới";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gold;
+            this.panel1.Controls.Add(this.lblName);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.lblVi);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.txtYourName);
+            this.panel1.Controls.Add(this.btnCopyEn);
+            this.panel1.Controls.Add(this.lblEn);
+            this.panel1.Controls.Add(this.btnCopyVi);
+            this.panel1.Controls.Add(this.txtVi);
+            this.panel1.Controls.Add(this.txtEn);
+            this.panel1.Location = new System.Drawing.Point(26, 23);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(544, 221);
+            this.panel1.TabIndex = 3;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.BlanchedAlmond;
+            this.ClientSize = new System.Drawing.Size(596, 267);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.Text = "Optimize Name";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtYourName;
+        private System.Windows.Forms.Label lblVi;
+        private System.Windows.Forms.TextBox txtVi;
+        private System.Windows.Forms.Button btnCopyVi;
+        private System.Windows.Forms.Label lblEn;
+        private System.Windows.Forms.TextBox txtEn;
+        private System.Windows.Forms.Button btnCopyEn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
