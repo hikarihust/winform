c4d4f046ffb331454bb431145c61fd2a2b1eb961
vudinhquang <vudinhquangk53@gmail.com>
2021-08-13 10:38:48 +0900

    Design

---
+++ 01-changeBackground/app-changeBackground/app-changeBackground/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace app_changeBackground
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.btnYellow = new System.Windows.Forms.Button();
+            this.btnRed = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.BackColor = System.Drawing.Color.Orange;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(31, 102);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(588, 51);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Hello WinForm";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnGreen.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGreen.Location = new System.Drawing.Point(37, 212);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(134, 35);
+            this.btnGreen.TabIndex = 1;
+            this.btnGreen.Text = "Green";
+            this.btnGreen.UseVisualStyleBackColor = false;
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.BackColor = System.Drawing.Color.Aqua;
+            this.btnBlue.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBlue.Location = new System.Drawing.Point(190, 212);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(134, 35);
+            this.btnBlue.TabIndex = 1;
+            this.btnBlue.Text = "Blue";
+            this.btnBlue.UseVisualStyleBackColor = false;
+            // 
+            // btnYellow
+            // 
+            this.btnYellow.BackColor = System.Drawing.Color.Gold;
+            this.btnYellow.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnYellow.Location = new System.Drawing.Point(348, 212);
+            this.btnYellow.Name = "btnYellow";
+            this.btnYellow.Size = new System.Drawing.Size(134, 35);
+            this.btnYellow.TabIndex = 1;
+            this.btnYellow.Text = "Yellow";
+            this.btnYellow.UseVisualStyleBackColor = false;
+            // 
+            // btnRed
+            // 
+            this.btnRed.BackColor = System.Drawing.Color.Red;
+            this.btnRed.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRed.Location = new System.Drawing.Point(495, 212);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(134, 35);
+            this.btnRed.TabIndex = 1;
+            this.btnRed.Text = "Red";
+            this.btnRed.UseVisualStyleBackColor = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LemonChiffon;
+            this.ClientSize = new System.Drawing.Size(653, 302);
+            this.Controls.Add(this.btnRed);
+            this.Controls.Add(this.btnYellow);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.btnGreen);
+            this.Controls.Add(this.lblTitle);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Change Background";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnGreen;
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button btnYellow;
+        private System.Windows.Forms.Button btnRed;
+    }
+}
+
