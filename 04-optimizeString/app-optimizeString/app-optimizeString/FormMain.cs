cfbd30319adb1856550e8d5588ae041e2f75a551
vudinhquang <vudinhquangk53@gmail.com>
2021-08-14 22:29:33 +0900

    Convert english

--- 04-optimizeString/app-optimizeString/app-optimizeString/FormMain.cs
+++ 04-optimizeString/app-optimizeString/app-optimizeString/FormMain.cs
@@ -61,11 +61,39 @@ namespace app_optimizeString
                 {
                     str = str.ToUpper();
                 }
+
+                // Xác định kiểu được chọn
+                if (radTypeNoAccent.Checked == true)
+                {
+                    str = RemoveAccent(str);
+                }
             }
             else
             {
                 MessageBox.Show("Dữ liệu không được rỗng", "Thông báo");
             }
+
+            return str;
+        }
+
+        private string RemoveAccent(string str)
+        {
+
+            str = Regex.Replace(str, @"(à|ả|ã|á|ạ|ă|ằ|ẳ|ẵ|ắ|ặ|â|ầ|ẩ|ẫ|ấ|ậ)", "a");
+            str = Regex.Replace(str, @"(è|ẻ|ẽ|é|ẹ|ê|ề|ể|ễ|ế|ệ)", "e");
+            str = Regex.Replace(str, @"(ò|ỏ|õ|ó|ọ|ô|ồ|ổ|ỗ|ố|ộ|ơ|ờ|ở|ỡ|ớ|ợ)", "o");
+            str = Regex.Replace(str, @"(ù|ủ|ũ|ú|ụ|ư|ừ|ử|ữ|ứ|ự)", "u");
+            str = Regex.Replace(str, @"(ì|ỉ|ĩ|í|ị)", "i");
+            str = Regex.Replace(str, @"(ỳ|ỷ|ỹ|ý)", "y");
+            str = Regex.Replace(str, @"(đ)", "d");
+
+            str = Regex.Replace(str, @"(À|Ả|Ã|Á|Ạ|Ă|Ằ|Ẳ|Ẵ|Ắ|Ặ|Â|Ầ|Ẩ|Ẫ|Ấ|Ậ)", "A");
+            str = Regex.Replace(str, @"(È|Ẻ|Ẽ|É|Ẹ|Ê|Ề|Ể|Ễ|Ế|Ệ)", "E");
+            str = Regex.Replace(str, @"(Ò|Ỏ|Õ|Ó|Ọ|Ô|Ồ|Ổ|Ỗ|Ố|Ộ|Ơ|Ờ|Ở|Ỡ|Ớ|Ợ)", "O");
+            str = Regex.Replace(str, @"(Ù|Ủ|Ũ|Ú|Ụ|Ư|Ừ|Ử|Ữ|Ứ|Ự)", "U");
+            str = Regex.Replace(str, @"(Ì|Ỉ|Ĩ|Í|Ị)", "I");
+            str = Regex.Replace(str, @"(Ỳ|Ỷ|Ỹ|Ý)", "Y");
+            str = Regex.Replace(str, @"(Đ)", "D");
 
             return str;
         }
