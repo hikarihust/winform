d04e4153b38ac93c3c83d80610eff4e4c5491013
vudinhquang <vudinhquangk53@gmail.com>
2021-08-15 14:55:46 +0900

    Add all source Create project App tính tiền lương

---
+++ 05-tinhluong/app-tinhluong/app-tinhluong/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_tinhluong
+{
+    class Helper
+    {
+        public static string FormatNumber(int value)
+        {
+            return String.Format("{0:n0}", value) + "đ";
+        }
+    }
+}
